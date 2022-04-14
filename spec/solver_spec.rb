12b3e8791ad4acb608d79c2987d6ae46aa8b2372
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-14 12:33:14 -0500

    Define first test case for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,1 +1,14 @@
+require_relative '../solver'
 
+describe Solver do
+  before :all do 
+    @solver = Solver.new
+  end
+  context 'Factorial Method' do
+    it 'should return 1 if 0 is provided' do
+      factorial = @solver.factorial(0)
+
+      expect(factorial).to eq 1
+    end
+  end
+end
