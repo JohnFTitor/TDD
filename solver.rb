eeac3d2d51540015f130c6888f12fa268aa2e01e
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-14 14:35:59 -0500

    Define methods to solve test cases

--- solver.rb
+++ solver.rb
@@ -15,7 +15,10 @@ class Solver
 	end
 
   def fizzbuzz(num)
-    'fizz'
+    return 'fizzbuzz' if num%5 === 0 && num%3 === 0
+    return 'fizz' if num%3 === 0
+    return 'buzz' if num%5 === 0
+    num.to_s
   end
 
 end
